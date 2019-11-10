076b8f7da1f3619f66aa49feac410b1073021e33
yrix90 <Ghjcnjgfhjkm981>
2019-11-10 10:25:43 +0700

    commenit

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'post': post})
 
 
 def post_detail(request, pk):
@@ -61,4 +61,17 @@ def post_remove(request, pk):
     post.delete()
     return redirect('post_list')
 
-# Create your views here.
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+    # Create your views here.
